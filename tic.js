6b55f4e46255b77deb841d54d75f6baccdba5fbf
M-Shareef-pg <najmussaqibpro@gmail.com>
2025-02-12 15:36:13 +0500

    commit

--- tic.js
+++ tic.js
@@ -3,6 +3,16 @@ let reset = document.querySelector(".reset")
 
 let player1 = true;
 
+const winPatterns = [
+    [0,1,2], //index 0
+    [0,3,6], //index 1
+    [0,4,8], //index 2
+    [1,4,7], //index 3
+    [2,5,8], //index 4
+    [2,4,6], //index 5
+    [3,4,5], //index 6
+    [6,7,8], //index 7
+];
 
 boxes.forEach((box) =>{ //loop for each box
     box.addEventListener("click", () =>{       //event listner
@@ -14,6 +24,12 @@ boxes.forEach((box) =>{ //loop for each box
             box.innerText = "O";
             player1 = true;
         }
+        box.disabled = true;
+        checkWinner();
     });
 
 });
+
+
+// check for winner
+
